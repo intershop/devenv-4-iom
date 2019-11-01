2ef90f928e97a059ea577add40c082c6704e6380
Andreas Herold <a.herold@intershop.de>
2019-11-01 13:20:06 +0100

    IOM-9732: added sales-demo specific template

--- templates/sales-demo-operations.sh
+++ templates/sales-demo-operations.sh
@@ -13,21 +13,37 @@ EOF
 
 case $1 in
     start)
-	start_iom
-	;;
+        start_iom
+        ;;
     stop)
-	stop_iom
-	;;
+        stop_iom
+        ;;
     free-storage)
-	free_storage
-	;;
+        free_storage
+        ;;
     *)
-	usage 1>&2
-	exit 1
+        usage 1>&2
+        exit 1
     ;;
 esac
 
 start_iom() {
-    kubectl create namespace ${EnvId}
+    echo "create namespace"
+    kubectl create namespace ${EnvId} || exit 1
+
+    echo "start mail server"
+    "${PROJECT_PATH}/scripts/template_engine.sh" "${PROJECT_PATH}/templates/mailhog.yml.template" "${ENV_DIR}/${CONFIG_FILE}" | kubectl apply --namespace ${EnvId} -f - || exit 1
+
+    echo "create local Docker volume"
+    docker volume create --name=${EnvId}-pgdata -d local || exit 1
+
+    echo "link Docker volume to database storage"
+    MOUNTPOINT="\"$(docker volume inspect --format='{{.Mountpoint}}' ${EnvId}-pgdata)\"" "${PROJECT_PATH}/scripts/template_engine.sh" "${PROJECT_PATH}/templates/postgres-storage.yml.template" "${ENV_DIR}/${CONFIG_FILE}" | kubectl apply --namespace ${EnvId} -f - || exit 1
+
+    echo "start postgres database"
+    "${PROJECT_PATH}/scripts/template_engine.sh" "${PROJECT_PATH}/templates/postgres.yml.template" "${ENV_DIR}/${CONFIG_FILE}" | kubectl apply --namespace ${EnvId} -f - || exit 1
+
+    echo "start IOM"
+    "${PROJECT_PATH}/scripts/template_engine.sh" "${PROJECT_PATH}/templates/iom.yml.template" "${ENV_DIR}/${CONFIG_FILE}" | kubectl apply --namespace ${EnvId} -f - || exit 1
 }
     
