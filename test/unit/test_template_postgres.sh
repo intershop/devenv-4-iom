faa64fe851d3cb19da9de822037b6ef9cf4d627d
Andreas Herold <a.herold@intershop.de>
2026-06-18 13:05:22 +0200

    fix: use /var/lib/postgresql as postgres mount point for all versions
    Mounting at /var/lib/postgresql works for both pg17 (data lands in data/)
    and pg18+ (data lands in a versioned subdirectory). Removes the version
    detection logic introduced in the previous commit.

--- test/unit/test_template_postgres.sh
+++ test/unit/test_template_postgres.sh
@@ -27,7 +27,7 @@ assert_not_contains "no PVC kind line" "$OUTPUT" "kind: PersistentVolumeClaim"
 assert_not_contains "no storageClassName" "$OUTPUT" "storageClassName:"
 
 test_case "volume mount present"
-assert_contains "volume mount included" "$OUTPUT" "mountPath: /var/lib/postgresql/data"
+assert_contains "volume mount included" "$OUTPUT" "mountPath: /var/lib/postgresql"
 
 test_case "service type is LoadBalancer"
 assert_contains "LoadBalancer service" "$OUTPUT" "type: LoadBalancer"
@@ -39,7 +39,6 @@ test_case "no unsubstituted variables"
 assert_not_contains "no raw POSTGRES_IMAGE" "$OUTPUT" '${POSTGRES_IMAGE}'
 assert_not_contains "no raw PostgresDataDirAbs" "$OUTPUT" '${PostgresDataDirAbs}'
 assert_not_contains "no raw IMAGE_PULL_POLICY_POSTGRES" "$OUTPUT" '${IMAGE_PULL_POLICY_POSTGRES}'
-assert_not_contains "no raw PostgresMountPath" "$OUTPUT" '${PostgresMountPath}'
 
 # relative path: should be resolved against PROJECT_DIR to an absolute path
 test_case "relative POSTGRES_DATA_DIR: resolved to absolute path"
@@ -59,17 +58,5 @@ OUTPUT_NODATA=$("$RENDER" --template="$TEMPLATE" --config=/dev/null --project-di
 assert_not_contains "no hostPath when unset" "$OUTPUT_NODATA" "hostPath:"
 assert_not_contains "no volumeMount when unset" "$OUTPUT_NODATA" "mountPath: /var/lib/postgresql/data"
 
-# postgres 18+: mount point moves up one level
-test_case "postgres:18 uses /var/lib/postgresql as mountPath"
-TMPDIR_PG18="$(mktemp -d)"
-echo "POSTGRES_IMAGE=postgres:18" > "$TMPDIR_PG18/test.properties"
-echo "POSTGRES_DATA_DIR=/tmp/test-pgdata" >> "$TMPDIR_PG18/test.properties"
-OUTPUT_PG18=$("$RENDER" --template="$TEMPLATE" --config="$TMPDIR_PG18/test.properties" --project-dir="$DEVENV_DIR" 2>&1)
-assert_contains "pg18 mountPath is /var/lib/postgresql" "$OUTPUT_PG18" "mountPath: /var/lib/postgresql"
-assert_not_contains "pg18 mountPath is not /data suffix" "$OUTPUT_PG18" "mountPath: /var/lib/postgresql/data"
-rm -rf "$TMPDIR_PG18"
-
-test_case "postgres:17 uses /var/lib/postgresql/data as mountPath"
-assert_contains "pg17 mountPath is /var/lib/postgresql/data" "$OUTPUT" "mountPath: /var/lib/postgresql/data"
 
 test_summary
